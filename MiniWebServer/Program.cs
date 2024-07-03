5b382c12a7e78852b966bf7e323028e3e00ec178
Buutyful <mantelli_git@libero.it>
2024-07-03 10:54:03 +0200

    Add project files.

---
+++ MiniWebServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var server = new TcpListener(IPAddress.Any, 6969);
+server.Start();
+
+while (true)
+{
+    var client = server.AcceptTcpClient();
+    var data = client.GetStream();
+
+    var buffer = new byte[1024];
+    var bytesRead = data.Read(buffer, 0, buffer.Length);
+
+    var request = Encoding.UTF8.GetString(buffer);
+
+    Console.WriteLine(request);
+    
+    var response =
+        "HTTP/1.1 200 OK\r\n" +
+        "Content-Type: text/plain\r\n" +
+        "Content-Length: " + bytesRead + "\r\n" +
+        "\r\n" + request;
+
+    var message = Encoding.UTF8.GetBytes(response);
+
+    data.Write(message);
+
+    client.Close();
+}
+
